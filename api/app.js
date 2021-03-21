b7afee8f487ded2ea470ccf952212171b7b09fe7
rodromart <rodromart@gmail.com>
2021-03-21 12:54:43 +0100

    first commit

---
+++ api/app.js
@@ -0,0 +1,11 @@
+async function main(){
+    const express   = require('express');
+    const app       = express();
+    const port      = 3000;
+
+    app.get('/', (req, res) => res.json({hello: 'world'}));
+    await app.listen(port);
+    console.log(`Listering on port ${port}.`);
+}
+
+main();
